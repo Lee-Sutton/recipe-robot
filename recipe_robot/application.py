f90d6c900dee435e3abf895818f9a98c814023e0
Lee Sutton <leesutton1@gmail.com>
2018-09-22 16:54:32 -0700

    created recipe and ingredient models

--- recipe_robot/application.py
+++ recipe_robot/application.py
@@ -1,9 +1,14 @@
 from flask import Flask
-from recipe_robot.api import api
 
 
 def create_app(app_name='RECIPE_ROBOT'):
     app = Flask(app_name)
     app.config.from_object('recipe_robot.config.BaseConfig')
+
+    from recipe_robot.api import api
     app.register_blueprint(api, url_prefix='/api')
+
+    from recipe_robot.models import db
+    db.init_app(app)
+
     return app
