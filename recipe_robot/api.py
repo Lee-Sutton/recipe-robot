bc7fee9dab41d99cc41bbb2a611016e6d7a18048
Lee Sutton <leesutton1@gmail.com>
2018-09-23 10:57:40 -0700

    added recipe api

--- recipe_robot/api.py
+++ recipe_robot/api.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from recipe_robot.models import Recipe, Ingredient, db
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello/<string:name>')
-def say_hello(name):
-    response = {'msg': f'Hello {name}'}
+@api.route('/ping/')
+def ping():
+    response = {'msg': 'pong'}
     return jsonify(response)
+
+
+@api.route('/recipes/', methods=('GET', 'POST'))
+def recipes():
+    if request.method == 'GET':
+        recipes = Recipe.query.all()
+        return jsonify({'recipes': [r.to_dict() for r in recipes]})
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        recipe = Recipe(name=data['name'])
+        ingredients = [Ingredient(name=i.name) for i in data['ingredients']]
+        recipe.ingredients = ingredients
+        db.session.add(recipe)
+        db.session.commit()
+        return jsonify(recipe.to_dict()), 201
+
+
+@api.route('/recipes/<int:id>/', methods=('GET', 'POST'))
+def recipe(id):
+    recipe = Recipe.query.get(id)
+    return jsonify({'recipe': recipe.to_dict()})
