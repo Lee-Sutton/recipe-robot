1d5bbda7c81678bf65b29970699b3b4ac0178042
Lee Sutton <leesutton1@gmail.com>
2018-09-24 08:56:29 -0700

    created flask test fixture

---
+++ recipe_robot/tests/test_api.py
@@ -0,0 +1,42 @@
+import os
+import pytest
+
+from recipe_robot.application import create_app
+from recipe_robot.models import db, Recipe
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app()
+    testing_client = flask_app.test_client()
+
+    # Establish an application context before running the tests.
+    ctx = flask_app.app_context()
+    ctx.push()
+
+    yield testing_client  # this is where the testing happens!
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='module')
+def init_database():
+    # Create the database and the database table
+    db.create_all()
+
+    # Insert user data
+    recipe1 = Recipe(name='Dummy Recipe')
+    db.session.add(recipe1)
+
+    # Commit the changes for the users
+    db.session.commit()
+
+    yield db  # this is where the testing happens!
+
+    db.drop_all()
+
+
+def test_get_recipes(test_client, init_database):
+    response = test_client.get('/api/recipes/')
+    assert response.status_code == 200
+    print(response)
