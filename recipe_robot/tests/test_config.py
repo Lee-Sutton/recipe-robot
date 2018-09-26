b45cf0fdc10fcd70aeb946224032882cd3cf65d9
Lee Sutton <leesutton1@gmail.com>
2018-09-25 18:47:25 -0700

    tested production and testing config

--- recipe_robot/tests/test_config.py
+++ recipe_robot/tests/test_config.py
@@ -9,33 +9,32 @@ class TestDevelopmentConfig(TestCase):
         self.app = create_app(config='recipe_robot.config.DevelopmentConfig')
         return self.app
 
-    def test_app_is_development(self):
+    def test_app_in_development_mode(self):
         assert self.app.config['SECRET_KEY'] == 'secretkey'
         assert current_app is not None
         assert (self.app.config['SQLALCHEMY_DATABASE_URI'] ==
                 os.environ.get('DATABASE_URL'))
 
 
-# class TestTestingConfig(TestCase):
-#     def create_app(self):
-#         app.config.from_object('project.config.TestingConfig')
-#         return app
-#
-#     def test_app_is_testing(self):
-#         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
-#         self.assertTrue(app.config['TESTING'])
-#         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-#         self.assertTrue(
-#             app.config['SQLALCHEMY_DATABASE_URI'] ==
-#             os.environ.get('DATABASE_TEST_URL')
-#         )
-#
-#
-# class TestProductionConfig(TestCase):
-#     def create_app(self):
-#         app.config.from_object('project.config.ProductionConfig')
-#         return app
-#
-#     def test_app_is_production(self):
-#         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
-#         self.assertFalse(app.config['TESTING'])
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        self.app = create_app(config='recipe_robot.config.TestingConfig')
+        return self.app
+
+    def test_app_in_test_mode(self):
+        assert self.app.config['SECRET_KEY'] == 'secretkey'
+        assert current_app is not None
+        assert self.app.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///test.db'
+
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        self.app = create_app(config='recipe_robot.config.ProductionConfig')
+        return self.app
+
+    def test_app_in_production_mode(self):
+        assert self.app.config['SECRET_KEY'] == 'secretkey'
+        assert current_app is not None
+        assert (self.app.config['SQLALCHEMY_DATABASE_URI'] ==
+                os.environ.get('DATABASE_URL'))
