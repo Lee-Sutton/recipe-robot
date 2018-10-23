478604766aba0bc8eea1f319f581f2e3490cb7a8
Lee Sutton <leesutton1@gmail.com>
2018-10-22 18:32:54 -0700

    created front end skeleton

--- services/client/src/App.vue
+++ services/client/src/App.vue
@@ -1,29 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="home" class="navbar-brand">Recipe Robot</router-link>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link to="home" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="about" class="nav-link">About</router-link>
+        </li>
+      </ul>
     </div>
-    <router-view/>
-  </div>
+  </nav>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
