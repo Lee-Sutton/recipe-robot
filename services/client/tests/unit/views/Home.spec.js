f7d136dbd829bd9d7fa9543db6a2a17ff6c752e6
Lee Sutton <leesutton1@gmail.com>
2018-11-09 23:55:00 -0500

    updated home tests

--- services/client/tests/unit/views/Home.spec.js
+++ services/client/tests/unit/views/Home.spec.js
@@ -1,9 +1,20 @@
-import { shallowMount } from '@vue/test-utils';
+import {shallowMount, RouterLinkStub} from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 
 describe('Home.vue', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Home, {
+            stubs: {
+                RouterLink: RouterLinkStub,
+            },
+        });
+    });
     it('renders the welcome message', () => {
-        const wrapper = shallowMount(Home);
         expect(wrapper.text()).toContain('Welcome to recipe buddy');
+    });
+
+    it('should link the user to the create an account page', () => {
+        const routerStub = wrapper.findAll(RouterLinkStub);
     });
 });
