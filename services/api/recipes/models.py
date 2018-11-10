f18906352e1dbdbc5d115d45a9d4094ee9d81d34
Lee Sutton <leesutton1@gmail.com>
2018-11-10 08:56:46 -0800

    created recipe model

--- services/api/recipes/models.py
+++ services/api/recipes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipes(models.Model):
+    """Recipes model"""
+    name = models.CharField(max_length=255)
+    amount = models.FloatField()
--- services/api/recipes/tests.py
+++ services/api/recipes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Recipes
 
-# Create your tests here.
+
+class TestRecipes(TestCase):
+    """Test suite for recipes model"""
+    def test_inserting_into_db(self):
+        """it should insert into the database"""
+        recipe = Recipes(name='test', amount=1)
+        recipe.save()
+        self.assertGreater(Recipes.objects.filter(name='test').count(), 0)
